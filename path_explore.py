df342de6dbc2666b6101967973ab3f63700cd2f9
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-03-03 17:12:29 -0500

    Represent program as dictionary, run through it assuming no branches.

--- path_explore.py
+++ path_explore.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
-# Path exploration for small code snippets.
+# Path exploration for individual functions
 
 # Read in file
 import sys
@@ -9,12 +9,31 @@ fh=sys.stdin
 import re
 dump_matcher = re.compile('\s*(?P<pc>[0-9a-f]+):\s*(?P<instruction>([0-9a-f]{2}\s)+)\s*(?P<opcode>[a-z]+)\s*(?P<arguments>.+)');
 
+# dictionary to represent program
+program = dict()
+
 # Read every line
 for line in fh.readlines():
   line.strip()
   match_op = dump_matcher.match(line);
   if (match_op != None):
-    print match_op.group('pc')
-    print match_op.group('instruction')
-    print match_op.group('opcode')
-    print match_op.group('arguments')
+    # Populate program dictionary
+    pc = int(match_op.group('pc'), 16)
+    instruction = match_op.group('instruction')
+    instruction_length = len(instruction.strip().split())
+    opcode = match_op.group('opcode')
+    args = match_op.group('arguments')
+    assert (pc not in program)
+    program[pc] = (opcode, args, instruction_length)
+
+# Make the last instruction point to a negative pc.
+program[pc] = (opcode, args, -2 * pc)
+
+# Start executing the program and building up paths
+paths = []
+print program
+assert(0 in program);
+pc = 0
+while(pc >= 0) :
+  print "Current PC: %x"%(pc)
+  pc += program[pc][2]
