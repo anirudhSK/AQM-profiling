6c6f6fbf2c300ee1da5605e5e28b6814947f4469
suvinay <suvinays89@gmail.com>
2014-03-04 00:24:25 -0500

    Intersperse source and relocations with disassembly

--- explore_paths.sh
+++ explore_paths.sh
@@ -3,5 +3,5 @@ algorithm=$1
 rm "$algorithm".o
 rm "$algorithm".check
 gcc -fno-exceptions -g -c "$algorithm".c
-objdump -w -j .text  -d "$algorithm".o > "$algorithm".check
+objdump -w -j .text -S -r -d "$algorithm".o > "$algorithm".check
 cat "$algorithm".check | python path_explore.py 0
